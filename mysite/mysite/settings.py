db6face78ffe9c17bbe1a181b3783df38ce2021e
Bruce Frick <bfrick22@gmail.com>
2017-05-17 00:08:43 -0700

    Adding polls app and migrations

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,11 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # local apps
+    'polls.apps.PollsConfig',
+
+    # thirdparty apps
+
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
+    # 'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
 
