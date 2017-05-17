bfa9794f64d17c89718c67b5cb08e61b9128fbd9
Bruce Frick <bfrick22@gmail.com>
2017-05-16 23:43:21 -0700

    adding pycharm to ignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='vegaspirates',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='bfrick',
+    author_email='',
+    description=''
+)
