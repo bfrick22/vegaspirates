b7b970a3495b374171f5df36054b5967b29d73c1
Bruce Frick <bfrick22@gmail.com>
2017-05-17 00:31:10 -0700

    Adding templates for polls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, get_list_or_404, render
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    questions = get_list_or_404(Question)
+    return render(request, 'polls/index.html', {'questions': questions})
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
