fc25fb8c2ca4cf35e9b23777bf889d763f798931
Bruce Frick <bfrick22@gmail.com>
2017-05-18 23:11:02 -0700

    Fixing header

--- mysite/mysite/context_processors.py
+++ mysite/mysite/context_processors.py
@@ -3,7 +3,10 @@ from django.conf import settings
 
 def navigation(request):
     context_processor = {k: '' for k in settings.NAV_URL_NAMES}
+
+    # active url - css class
     if request.resolver_match.app_names:
         for i in request.resolver_match.app_names:
             context_processor[i] = 'active'
+
     return context_processor
