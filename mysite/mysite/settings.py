8021693a00610d554fb46a290cf9abef7e17100f
Bruce Frick <bfrick22@gmail.com>
2017-05-24 23:01:20 -0700

    Adding check for cache test environment

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,6 +121,9 @@ CACHES = {
         'LOCATION': 'lowcash',
     }
 }
+
+if 'test' in sys.argv:
+    CACHE_MIDDLEWARE_SECONDS = 0
 
 
 # Password validation
