4be85042ded6980fbfa72a28121f86c182e1631c
Bruce Frick <bfrick22@gmail.com>
2017-06-04 22:47:27 -0700

    adding linkify tool for tiwtter feeds

--- mysite/mysite/templatetags/template_tools.py
+++ mysite/mysite/templatetags/template_tools.py
@@ -14,8 +14,8 @@ def obj_debug(value):
 
 @register.filter(name='linkify')
 def linkify(value):
-    value = str(value)
-    reg = u'(?P<link> http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)'
-    output = re.sub(reg, "<a href=''></a>", value)
-    print output
-    return value
+    urlfinder = re.compile(u'(?P<link> http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)')
+    match = urlfinder.sub(r'<a href="\1">\1</a>', value)
+    if not match:
+        return value
+    return match
